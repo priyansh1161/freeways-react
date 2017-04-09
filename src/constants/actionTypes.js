c823e344d00ce05ff19b79c49660933b6ad279b9
priyansh gupta <priyanshgupta1161@gmail.com>
2017-04-09 16:25:00 +0530

    login action added

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,4 @@
 export const BIKE_LOAD_SUCCESS = 'BIKE_LOAD_SUCCESS';
 export const BIKE_LOAD_FAILURE = 'BIKE_LOAD_FAILURE';
 export const ADD_TO_CART = 'ADD_TO_CART';
+export const LOG_IN = 'LOG_IN';
